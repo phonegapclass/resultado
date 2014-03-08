1df3422bd7f87b4db082b6db2e7db47cc62a6f0a
Carlos Celma <solrac@igitsoft.com>
2014-03-08 17:30:12 -0600

    n

---
+++ js/actions.js
@@ -0,0 +1,73 @@
+//actions
+var jQT = new $.jQTouch({
+	themeSelectionSelector: '#jqt'
+});
+
+function escribir(texto){
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+    // device APIs are available
+    //
+    function onDeviceReady() {
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+    }
+
+    function gotFS(fileSystem) {
+        fileSystem.root.getFile("practica8.txt", {create: true, exclusive: false}, gotFileEntry, fail);
+    }
+
+    function gotFileEntry(fileEntry) {
+        fileEntry.createWriter(gotFileWriter, fail);
+    }
+
+    function gotFileWriter(writer) {
+        writer.onwriteend = function(evt) {
+            navigator.notification.alert("El archivo fue escrito satisfactoriamente.",null,"Escribir","Aceptar");
+        };
+        writer.write(text);
+    }
+
+    function fail(error) {
+        alert(error.code);
+    }
+}
+
+function leer(){
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+    // device APIs are available
+    //
+    function onDeviceReady() {
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+    }
+
+    function gotFS(fileSystem) {
+        fileSystem.root.getFile("practica8.txt", null, gotFileEntry, fail);
+    }
+
+    function gotFileEntry(fileEntry) {
+        fileEntry.file(readAsText, fail);
+    }
+
+    function readAsText(file) {
+        var reader = new FileReader();
+        reader.onloadend = function(evt) {
+            $('#arcLeer').val(evt.target.result);
+        };
+        reader.readAsText(file);
+    }
+
+    function fail(evt) {
+        console.log(evt.target.error.code);
+    }
+}
+
+$(function(){
+    $('#archivos .individual li').tap(function(){
+        if($(this).index()==0){//Escribir
+            escribir($('#arcEscribir').val());
+        }else{//Leer
+            leer();
+        }
+    });
+});
